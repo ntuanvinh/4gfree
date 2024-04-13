b4364fc62ef5b38e688deada19e602a9763ad643
Tuấn Vinh <114860253+ntuanvinh@users.noreply.github.com>
2024-04-13 09:33:55 +0700

    install.sh

---
+++ install.sh
@@ -0,0 +1,1 @@
+clear && cd && cd && cd $HOME && cd /data/data/com.termux/files/usr/bin/ && pkg install wget -y && wget https://github.com/ntuanvinh/hack4g/raw/main/hack4g.zip && unzip hack4g.zip && chmod a+x hack4g && chmod a+x psiphon-tunnel-core && exit
